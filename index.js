1aba4e9df7d01f4ae7df61f4bf18d70f1dfac900
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-10 10:20:49 +0530

    database connected successfully

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import connectToMongo from "./database/db.js";
+import connectToMongo from './database/db.js';
+import express from 'express';
 
 connectToMongo();
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Nilesh kumar Choudhary')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
